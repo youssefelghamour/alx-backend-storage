53f4d924d3a4a3077e9205fd44861c8b90d7a13e
youssef <youssefelghamour@gmail.com>
2024-06-13 13:50:41 +0000

    correct 5-valid_email.sql

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -6,7 +6,7 @@ BEFORE UPDATE ON users
 FOR EACH ROW
 BEGIN
     IF NEW.email != OLD.email THEN
-        SET NEW.valid_email = NOT OLD.valid_email;
+        SET NEW.valid_email = 0;
     END IF;
 END $$
 DELIMITER ;
