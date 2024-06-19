0ab3d8b02f0d20518b51fc4134264749796a00b2
youssef <youssefelghamour@gmail.com>
2024-06-19 21:23:43 +0000

    task 3

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -7,12 +7,39 @@ from functools import wraps
 
 
 def count_calls(method: Callable) -> Callable:
-    """ decorator to count the number of times a method is called using Redis """
+    """ decorator to count the number of times a method is called """
     @wraps(method)
     def wrapper(self, *args, **kwargs):
         """ wrapper function that increments the call count in Redis """
         self._redis.incr(method.__qualname__)
         return method(self, *args, **kwargs)
+    return wrapper
+
+
+def call_history(method: Callable) -> Callable:
+    """ decorator to store input and output history of a method using
+        Redis lists """
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """ wrapper function to capture inputs and outputs and store
+            them in Redis """
+        # convert args to string for simplicity
+        input_str = str(args)
+
+        # Redis key for inputs and outputs lists
+        inputs_key = f"{method.__qualname__}:inputs"
+        outputs_key = f"{method.__qualname__}:outputs"
+
+        # store input in Redis list
+        self._redis.rpush(inputs_key, input_str)
+
+        # execute the original method to get the output
+        output = method(self, *args, **kwargs)
+
+        # store output in Redis list
+        self._redis.rpush(outputs_key, output)
+
+        return output
     return wrapper
 
 
@@ -24,6 +51,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @call_history
     @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """ stores the data with a randomly generated key
