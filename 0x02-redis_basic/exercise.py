6387dcad11488725be44acf53d126bd30d82e1b2
youssef <youssefelghamour@gmail.com>
2024-06-19 20:41:16 +0000

    task 1

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,7 +2,7 @@
 """ module for Cache class """
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable
 
 
 class Cache:
@@ -19,3 +19,21 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Callable = None) -> Union[str, bytes,
+                                                          int, float]:
+        """ retrieves the data for a key
+            and optionally applies a function to convert it """
+        value = self._redis.get(key)
+        if fn:
+            return fn(value)
+        else:
+            return value
+
+    def get_str(self, key: str) -> str:
+        """ retrieves the data for a key and converts it to a string """
+        return self.get(key, lambda d: d.decode('utf-8'))
+
+    def get_int(self, key: str) -> int:
+        """ retrieves the data for a key and converts it to an int """
+        return self.get(key, lambda d: int(d))
